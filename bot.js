f4581bc492d338e09a68a134e4d2efd29bbd2680
anis5555 <47055330+anis5555@users.noreply.github.com>
2019-02-03 11:28:45 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get(538671092626358292)
+let channel =     client.channels.get("538671092626358292")
 setInterval(function() {
-channel.send(احححححححححح سبام رهيب يا زق ايم ذو بسط فورر ايفر);
+channel.send(`هنا احححححححححح سبام رهيب يا زق ايم ذو بسط فورر ايفركلام ال سبام`);
 }, 30)
 })
 
